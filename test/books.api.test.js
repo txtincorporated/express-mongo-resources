1776f30c2d8457f59213010310668302f73a7a48
Nathan P. Keene <npk@txtincorporated.com>
2016-11-04 01:00:52 -0700

    Express-mongo-resources all functions operational and passing tests on one resource

--- test/books.api.test.js
+++ test/books.api.test.js
@@ -29,12 +29,19 @@ describe('book', () => {
 
   const request = chai.request(app);
 
-  // const sawyer = {
-  //   title: 'Tom Sawyer',
-  //   author: 'Mark Twain'
-  // }; 
+  const sawyer = {
+    title: 'Tom Sawyer',
+    author: 'Mark Twain'
+  }; 
+  const clemens = {
+    title: 'Tom Sawyer',
+    author: 'Samuel Clemens',
+  };
 
-  it('/GET all', done => {
+  var bookResult = null;
+
+
+  it('/GET all -- before', done => {
     request
       .get('/api/books')
       .then(res => {
@@ -43,5 +50,75 @@ describe('book', () => {
       })
       .catch(done);
   });
-  // after(done => connection.close(done));
+
+  it('/POST', done => {
+    request
+      .post('/api/books')
+      .send(sawyer)
+      .then(res => {
+        bookResult = res.body;
+        console.log('bookResult POST: ', bookResult);
+        sawyer.__v = 0;
+        sawyer._id = bookResult._id;
+        assert.deepEqual(bookResult, sawyer);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('/GET/:id', done => {
+    request
+      .get('/api/books')
+      .query({_id:sawyer._id})
+      .then(res => {
+        console.log('bookResult GET', bookResult);
+        console.log('sawyer GET: ', sawyer);
+        bookResult = res.body;
+        assert.deepEqual(bookResult, [sawyer]);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('/PUT/:id', done => {
+    request
+      .put(`/api/books/${sawyer._id}`)
+      // .query({_id:sawyer._id})
+      .send(clemens)      
+      .then(res => {
+        console.log('bookResult PUT1', bookResult);
+        console.log('clemens1: ', clemens);
+        bookResult = res.body;
+        console.log('bookResult PUT2', bookResult);
+        clemens.__v = 0;
+        clemens._id = bookResult._id;
+        console.log('clemens2: ', clemens);
+        assert.deepEqual(bookResult, clemens);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('GET all -- after', done => {
+    request
+      .get('/api/books')
+      .then(res => {
+        assert.deepEqual(res.body, [clemens]);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('DELETE /:id', done => {
+    request
+      .delete('/api/books')
+      .query({_id:sawyer._id})      
+      .then(res => {
+        bookResult = res.body;
+        assert.deepEqual(bookResult, {});
+        done();
+      })
+      .catch(done);
+  });
+
 });
